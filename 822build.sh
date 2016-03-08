b36fded2a728238fdfedbbbbcb5400f024161af4
Keerthan Jaic <jckeerthan@gmail.com>
2016-03-08 06:08:16 -0500

    add build script

---
+++ 822build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export MAKEFLAGS="-j$(nproc)"
+
+if [ ! -f .config ] || ! cmp -s .config config.822; then
+  echo "Copying config.822 to .config"
+  cp config.822 .config
+  make oldconfig
+fi
+
+make bzImage && make modules && make modules_install && make install
